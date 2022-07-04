0e9212ba5e6fbc257622c4f18370e85b1b97aa5d
bran.nie <pengcheng1995@gmail.com>
2022-07-04 13:48:16 +0800

    line

--- js/DesignPatterns/Observer/index.js
+++ js/DesignPatterns/Observer/index.js
@@ -1,33 +1,33 @@
 class Subject {
     constructor() {
-        this.observers = []
+        this.observers = [];
     }
 
     add(observer) {
-        this.observers.push(observer)
+        this.observers.push(observer);
     }
 
     notify(...args) {
-        this.observers.forEach(observer => observer.update(...args))
+        this.observers.forEach((observer) => observer.update(...args));
     }
 }
 
 class Observer {
     update(...args) {
-        console.log(...args)
+        console.log(...args);
     }
 }
 
 // 创建观察者
-let ob1 = new Observer()
-let ob2 = new Observer()
+let ob1 = new Observer();
+let ob2 = new Observer();
 
 // 创建目标对象, 存储观察者对象
-let sub = new Subject()
+let sub = new Subject();
 
 // 将目标和观察者建立了依赖关系
-sub.add(ob1)
-sub.add(ob2)
+sub.add(ob1);
+sub.add(ob2);
 
 // 当状态变更时, 主动通知观察者, 发送信息
-sub.notify('给观察者们发送信息')
+sub.notify('给观察者们发送信息');
--- js/DesignPatterns/Publisher-Subscriber/index.js
+++ js/DesignPatterns/Publisher-Subscriber/index.js
@@ -1,40 +1,38 @@
 class PubSub {
     constructor() {
-        this.subscribers = []
+        this.subscribers = {};
     }
 
     subscribe(type, fn) {
         if (typeof this.subscribers[type] === 'undefined') {
-            this.subscribers[type] = [fn]
+            this.subscribers[type] = [fn];
         } else {
-            this.subscribers[type].push(fn)
+            this.subscribers[type].push(fn);
         }
     }
 
     publish(type, ...args) {
-        let fns = this.subscribers[type] || []
-        fns.forEach(fn => fn(...args))
+        let fns = this.subscribers[type] || [];
+        fns.forEach((fn) => fn(...args));
     }
 }
 
 // 创建事件调度中心, 为订阅者和发布者提供调度服务
-let pubSub = new PubSub()
+let pubSub = new PubSub();
 
 // A订阅了SMS事件, (和观察者不同的是, 发布订阅模式中, 订阅者只关注事件本身, 也就是A只关于SMS本身, 而不关心是谁发布了这个事件
-pubSub.subscribe('sms', console.log)
+pubSub.subscribe('sms', console.log);
 
 // B订阅了SMS事件,
-pubSub.subscribe('sms', console.log)
+pubSub.subscribe('sms', console.log);
 
 // C 订阅了Email事件
-pubSub.subscribe('email', console.log)
-
+pubSub.subscribe('email', console.log);
 
 // 调度中心发布事件,   发布 SMS Email, phone 事件
-pubSub.publish('sms', 'I publish `sms` event')
-pubSub.publish('email', 'I publish `email` event')
-pubSub.publish('phone', 'I publish `phone` event')
-
+pubSub.publish('sms', 'I publish `sms` event');
+pubSub.publish('email', 'I publish `email` event');
+pubSub.publish('phone', 'I publish `phone` event');
 
 /*
     举个生活中的范例:
@@ -44,42 +42,39 @@ pubSub.publish('phone', 'I publish `phone` event')
     和观察者模式不太同的地方是, 爱豆不需要知道都有哪些人关注了自己, 只需要发动态就完了,  粉丝和爱豆并没有直接消息关联.
  */
 
-
-
-
 // 这是es5的写法
 const Observe = (function () {
     // 防止消息队列暴露而被篡改，将消息容器设置为私有变量
-    let __message = {}
+    let __message = {};
     return {
         // 注册消息接口
         on: function (type, fn) {
             // 如果此消息不存在，创建一个该消息类型     数组
             if (typeof __message[type] === 'undefined') {
-                __message[type] = [fn]
+                __message[type] = [fn];
             } else {
                 // 将执行方法推入该消息对应的执行队列中
-                __message[type].push(fn)
+                __message[type].push(fn);
             }
         },
 
         // 订阅消息接口
         subscribe: function (type, args) {
             // 如果该消息没有注册，直接返回
-            if (!__message[type]) return
+            if (!__message[type]) return;
 
             // 定义消息信息
             let events = {
-                    type: type,                     // 消息类型
-                    args: args || {}                // 参数
+                    type: type, // 消息类型
+                    args: args || {}, // 参数
                 },
-                i = 0,                          // 循环变量
-                len = __message[type].length    // 执行队列长度
+                i = 0, // 循环变量
+                len = __message[type].length; // 执行队列长度
 
             // 遍历执行函数
             for (; i < len; i++) {
                 // 依次执行注册消息对应的方法
-                __message[type][i].call(this, events)
+                __message[type][i].call(this, events);
             }
         },
 
@@ -88,28 +83,28 @@ const Observe = (function () {
             // 如果消息执行队列存在
             if (__message[[type] instanceof Array]) {
                 // 从最后一条依次遍历
-                let i = __message[type].length - 1
+                let i = __message[type].length - 1;
                 for (; i >= 0; i--) {
                     // 如果存在该执行函数, 则移除相应的动作
-                    __message[type][i] === fn && __message[type].slice(i, 1)
+                    __message[type][i] === fn && __message[type].slice(i, 1);
                 }
             }
-        }
-    }
-})()
+        },
+    };
+})();
 
 // 订阅消息
 Observe.on('say', function (data) {
-    console.log(data.args.text)
-})
+    console.log(data.args.text);
+});
 Observe.on('say', function (data) {
-    console.log('second: the type is ', data.type)
-})
+    console.log('second: the type is ', data.type);
+});
 Observe.on('success', function () {
-    console.log('success')
-})
+    console.log('success');
+});
 
 // 发布消息
-Observe.subscribe('say', {text: 'hello world'})
-Observe.subscribe('say', {})
-Observe.subscribe('success')
+Observe.subscribe('say', { text: 'hello world' });
+Observe.subscribe('say', {});
+Observe.subscribe('success');
