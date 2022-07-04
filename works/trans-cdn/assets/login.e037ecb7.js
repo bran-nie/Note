a8e10cfb4a9942f0c848e840d35213183af682ee
bran.nie <pengcheng1995@gmail.com>
2022-07-04 14:03:53 +0800

    transform cdn data

--- works/trans-cdn/assets/login.e037ecb7.js
+++
@@ -1,1 +0,0 @@
-import{j as e,c as t,R as n}from"./jsx-runtime.56dcf443.js";function o(){return e("div",{children:"login"})}t.createRoot(document.getElementById("root")).render(e(n.StrictMode,{children:e(o,{})}));
